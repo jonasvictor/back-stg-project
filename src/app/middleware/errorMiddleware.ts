5f985839dfad6893b4fc1ff7d0141868016c492d
Jonas Victor <jonas.silva@servicenet.com>
2023-07-13 14:53:44 -0300

    Adicionando o arquivo errorMiddleware, melhorando o tratamendo de erros da aplicação

---
+++ src/app/middleware/errorMiddleware.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from "express";
+import { ApiError } from "../../helpers/api-erros";
+
+export const errorMiddleware = (error: Error & Partial<ApiError>, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = error.statusCode ?? 500;
+    const message = error.message ?? 'Internal server error';
+    return res.status(statusCode).json({ message });
+}
