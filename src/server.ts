751f1102869afd19a3cfb68b0df6f8a86648b3d9
Jonas Victor <jonas.silva@servicenet.com>
2023-07-13 15:04:01 -0300

    Atuazando o arquivo server.ts adicionando o errorMiddleware

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import 'reflect-metadata';
-import express from 'express';
+import 'express-async-errors';
 import cors from 'cors';
 import { AppDataSource } from './database/data-source';
 import routers from './app/routes/routes';
+import express from 'express';
+import { errorMiddleware } from './app/middleware/errorMiddleware';
 
 const app = express();
 
@@ -13,6 +15,11 @@ app.use(express.json());
 app.use(routers)
 
 AppDataSource.initialize().then(async () => {
+    // app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    //     console.log(error);
+    //     return res.json('Caiu no middleware de erro.')
+    // })
+    app.use(errorMiddleware)
     console.log('Database connected');
     app.listen(process.env.PORT, () => {
         console.log(`Server started on port ${process.env.PORT}`);
