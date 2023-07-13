135c16a184a0f8426606fd8821b50dce7376aa97
Jonas Victor <jonas.silva@servicenet.com>
2023-07-13 15:18:53 -0300

    Atuazando o arquivo Usuario.ts

--- src/app/entities/Usuario.ts
+++ src/app/entities/Usuario.ts
@@ -1,18 +1,19 @@
-import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate } from "typeorm";
+import { Entity, Column, PrimaryGeneratedColumn, BeforeInsert, BeforeUpdate, Unique } from "typeorm";
 import bcrypt from 'bcryptjs';
 
 @Entity('usuario')
+@Unique(['email'])
 class Usuario {
     @PrimaryGeneratedColumn('increment')
     id: number;
 
-    @Column('varchar', {length: 100})
+    @Column('varchar', { length: 100 })
     name: string;
 
-    @Column('varchar', {length: 255})
+    @Column('varchar', { length: 255 })
     email: string;
 
-    @Column('varchar', {length: 255})
+    @Column('varchar', { length: 255 })
     senha: string;
 
     @BeforeInsert()
@@ -21,6 +22,16 @@ class Usuario {
         this.senha = bcrypt.hashSync(this.senha, 8);
     }
 
+    // @BeforeInsert()
+    // @BeforeUpdate()
+    // validateFields() {
+    //     if (!this.name) {
+    //         throw new Error("O campo 'name' é obrigatório.");
+    //     }
+    //     if (!this.email) {
+    //         throw new Error("O campo 'email' é obrigatório.");
+    //     }
+    // }
 }
 
 export default Usuario;
