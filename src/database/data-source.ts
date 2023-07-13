8799f005bba8fe778c020810800af1324165f95b
Jonas Victor <jonas.silva@servicenet.com>
2023-07-13 15:09:09 -0300

    Atuazando o arquivo data-source.ts

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -3,20 +3,16 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import Usuario from "../app/entities/Usuario"
 
-// const port = process.env.DB_PORT as number | undefined
-
 export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST,
     port: process.env.DB_PORT as number | undefined,
-    // port: Number(process.env.POSTGRES_PORT),
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    // synchronize: false,
-    // logging: true,
-    // entities: [`${__dirname}/**/entities/*.{ts,js}`],
+    logging: true,
     entities: [Usuario],
+    // synchronize: false,
     // migrations: [],
     // subscribers: [],
 })
