74880198235857f0393461e0ae24b4b883ed9c83
Jonas Victor <jonas.silva@servicenet.com>
2023-07-27 00:12:50 -0300

    Adicionando novas rotas no arquivo routes

--- src/app/routes/routes.ts
+++ src/app/routes/routes.ts
@@ -1,34 +1,44 @@
 import { Router } from "express";
 import usuarioRouter from "../controllers/UsuarioController";
 import AuthController from "../controllers/AuthController";
-import IndexController from "../controllers/IndexController";
 import { authMiddleware } from "../middleware/authMiddleware";
+import TransacaoController from "../controllers/TransacaoController";
 
 const routers = Router();
 
-// routers.get('/', async (req, res, next) => {
-//     throw new BadRequestError('Erro lançado do API Error.');
-//     // try {
-//     //     throw new UnauthorizedError('Erro lançado do ApiError.');
-//     // } catch (error) {
-//     //     next(error);
-//     // }
-    
-//     // return res.json('OK');
-//     // try {
-//     //     // const file = fs.readFileSync('teste.txt');
-//     //     // return res.json(file.toString());
-//     //     throw new Error('Erro de teste.');
-//     // } catch (error) {
-//     //     // return res.status(500).json(error);
-//     //     console.log(error);
-//     // }
-// });
-
-routers.use('/usuarios', usuarioRouter);
+// Rota para login e logout
 routers.use('/login', AuthController.login);
 routers.use('/logout', AuthController.logout);
+
+// Rota para criar um novo usuário, listar todos os usuários e buscar um usuário pelo ID, atualizar e excluir um usuário
+routers.use('/usuarios', usuarioRouter);
+
+// Rotas para as transações
+routers.post('/transacoes', TransacaoController.createTransacaoController);
+routers.get('/transacoes/:id', TransacaoController.getTransacaoByIdController);
+routers.put('/transacoes/:id', TransacaoController.updateTransacaoStatusController);
+routers.delete('/transacoes/:id', TransacaoController.deleteTransacaoController);
+
+// Rota para realizar o depósito
+routers.post('/transacoes/deposito', TransacaoController.createDepositoController);
+
+// Rota para realizar o saque
+routers.post('/transacoes/saque', TransacaoController.createSaqueController);
+
+// Rota para obter o saldo do usuário
+routers.get('/saldo/:usuario_id', TransacaoController.getSaldoUsuarioController);
+
+// Rota para obter o extrato do usuário
+routers.get('/extrato/:usuario_id', TransacaoController.getExtratoTransacoesUsuarioController);
+
+// Rota para obter o extrato do usuário (depósito)
+routers.get('/extrato/deposito/:usuario_id', TransacaoController.getExtratoDepositoUsuarioController);
+
+// Rota para obter o extrato do usuário (saque)
+routers.get('/extrato/saque/:usuario_id', TransacaoController.getExtratoSaqueUsuarioController);
+
+
+// Rota para obter o perfil do usuário autenticado
 routers.use('/perfil', authMiddleware, AuthController.getPerfil);
-routers.use('/usuario', authMiddleware, IndexController.index);
 
 export default routers;
