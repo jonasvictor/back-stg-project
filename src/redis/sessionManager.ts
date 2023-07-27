2f7d0d9637f386a5a33f4443fd707d0fdca42038
Jonas Victor <jonas.silva@servicenet.com>
2023-07-26 23:40:41 -0300

    Ajustes no arquivo do redis

--- src/redis/sessionManager.ts
+++ src/redis/sessionManager.ts
@@ -1,27 +1,32 @@
 import { createClient } from 'redis';
 
+// Cria o cliente do Redis
 const client = createClient({
-    url: 'redis://localhost:6379'
+  url: 'redis://localhost:6379'
 });
-  
+
 client.on('error', err => console.log('Redis Client Error', err));
+
 client.connect();
 
+// Função para obter uma sessão do Redis
 export async function getSession(key = '') {
-    return await client.hGetAll(key);
+  return await client.hGetAll(key);
 }
 
+// Função para definir uma sessão no Redis
 export async function setSession(key = '', value = {}) {
-    await client.hSet(key, value)
+  await client.hSet(key, value);
 }
 
+// Função para obter o token de autorização
 export function getToken(headers: any) {
-    const token = headers['authorization'].split(' ')[1];
-    return token
+  const token = headers['authorization'].split(' ')[1];
+  return token;
 }
 
+// Função para remover uma sessão do Redis
 export async function removeSession(key = '') {
-    await client.del(key);
-
-    return true;
+  await client.del(key);
+  return true;
 }
